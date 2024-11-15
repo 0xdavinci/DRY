955179c81e38fe0076d6e4b1e751473f8ffa32da
d@vinc! <160800039+0xdavinci@users.noreply.github.com>
2024-11-14 19:52:08 -0500

    1# Use Functions to Encapuslate Reusable Logic

---
+++ Encapuslate/After.js
@@ -0,0 +1,12 @@
+const user1Name ="Brian Howell";
+const user2Name = "James Hong";
+
+function greetUser(name) {
+    return `Hello, ${name}! Welcome back.`;
+}
+
+let greetUser1 = greetUser(user1Name);
+let greetUser2 = greetUser(user2Name);
+
+console.log(greetUser1);
+console.log(greetUser2);
---
+++ Encapuslate/Before.js
@@ -0,0 +1,8 @@
+const user1Name = "Brian Howell";
+const user2Name = "James Hong";
+
+let greetUser1 = `Hello, ${user1Name}! Welcome back.`;
+let greetUser2 = `Hello, ${user2Name}! Welcome back.`;
+
+console.log(greetUser1);
+console.log(greetUser2);
