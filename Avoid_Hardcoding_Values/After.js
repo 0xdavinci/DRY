0394446a5ae3b44de03d791da3a6df4b0ce2f596
d@vinc! <160800039+0xdavinci@users.noreply.github.com>
2024-11-14 20:13:42 -0500

    3# Avoid Hardcoding Values

---
+++ Avoid_Hardcoding_Values/After.js
@@ -0,0 +1,11 @@
+const PI = 3.14159;
+
+function areaCircle(radius) {
+    return PI * radius * radius;
+}
+
+let areaCircle1 = areaCircle(2);
+let areaCircle2 = areaCircle(3);
+
+console.log(areaCircle1);
+console.log(areaCircle2);
---
+++ Avoid_Hardcoding_Values/Before.js
@@ -0,0 +1,7 @@
+const radius1 = 2;
+const radius2 = 3;
+let areaCircle1 = 3.14159 * radius1 * radius1;
+let areaCircle2 = 3.14159 * radius2 * radius2;
+
+console.log(areaCircle1);
+console.log(areaCircle2);
