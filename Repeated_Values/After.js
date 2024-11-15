eee9acdbca74103f2c0b3e08835f6227a98840d3
d@vinc! <160800039+0xdavinci@users.noreply.github.com>
2024-11-14 20:04:12 -0500

    2# Use Constants for Repeated Values

---
+++ Repeated_Values/After.js
@@ -0,0 +1,5 @@
+const TAX_RATE = 0.07;
+let subtotal = 2;
+let totalWithTax = subtotal + subtotal *TAX_RATE;
+
+console.log(totalWithTax);
---
+++ Repeated_Values/Before.js
@@ -0,0 +1,5 @@
+let taxRate = 0.07;
+let subtotal = 2;
+let totalWithTax = subtotal + subtotal * 0.07;
+
+console.log(totalWithTax);
